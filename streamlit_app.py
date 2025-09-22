ef67b7571ae2f8b89ef8254cd663c732e0a0ec5f
rafaelr-s <rafaelrnlog@gmail.com>
2025-09-22 10:11:49 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,6 @@ def gerar_pdf(cliente, vendedor, itens_confeccionados, itens_bobinas, resumo_con
     pdf.cell(200, 6, "CLIENTE", ln=True)
     pdf.set_font("Arial", size=9)
     pdf.multi_cell(200, 5, f"Nome/Razão: {cliente.get('nome','')}")    
-   # Mostra CNPJ ou CPF somente se informado
     cnpj_cpf = cliente.get("cnpj", "").strip()
     if cnpj_cpf:
         pdf.multi_cell(200, 5, f"CNPJ/CPF: {cnpj_cpf}")
